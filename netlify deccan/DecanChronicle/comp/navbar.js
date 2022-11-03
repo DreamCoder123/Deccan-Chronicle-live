80a3e909f056547f5c9db44fea17f27f1bad1699
Mohammed Ashique <mohammedashique127@gmail.com>
2022-11-03 11:20:47 +0530

    deccan live

---
+++ netlify deccan/DecanChronicle/comp/navbar.js
@@ -0,0 +1,24 @@
+let navbar = ()=>{
+
+    return `<img src="DeccanChronicle_Logo.jpg"  id="aslogo" alt="logo">
+    <div id="time">
+    </div>
+    <hr class="line">
+    <div id="asnavmenu">
+      
+   <a href="./index.html"> <img src="https://cdn-icons-png.flaticon.com/512/25/25694.png" alt="home icon" id="home-icon"></a>
+      <a href="./nation.html">Nation</a>
+      <a href="./world.html">World</a>
+      <a href="./entertainment.html">Entertainment</a>
+      <a href="./job.html">Jobs & Education</a>
+      <a href="./sports.html">Sports</a>
+      <a href="./technology.html">Technology</a>
+      <a href="./lifestyle.html">Lifestyle</a>
+      <a href="./gallery_sub_page.html">Gallery</a>
+      <a href="/">More...</a>
+    </div>
+    <hr>`
+ 
+}
+
+export {navbar}
